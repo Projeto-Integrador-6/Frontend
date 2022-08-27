af32009276aeacab47ffc7890aeac23960c25067
adailson4s <adailson.up@gmail.com>
2022-08-27 15:35:41 -0300

    logica da tela "Splash"

--- app/src/main/java/com/pi/ativas/splash/SplashFragment.kt
+++ app/src/main/java/com/pi/ativas/splash/SplashFragment.kt
@@ -1,11 +1,21 @@
 package com.pi.ativas.splash
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.findFragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.pi.ativas.R
 import com.pi.ativas.databinding.FragmentSplashBinding
+import com.pi.ativas.login.LoginFragment
 
 class SplashFragment : Fragment() {
 
@@ -13,6 +23,10 @@ class SplashFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+        },2000)
 
     }
 
@@ -22,6 +36,5 @@ class SplashFragment : Fragment() {
     ): View {
         binding = FragmentSplashBinding.inflate(layoutInflater)
         return binding.root
-
     }
 }
