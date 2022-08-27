19f2a90f75d6e76fc299775b925d9b8522b74d2d
adailson4s <adailson.up@gmail.com>
2022-08-27 18:34:43 -0300

    Ajuste na tela Login

--- app/src/main/java/com/pi/ativas/splash/SplashFragment.kt
+++ app/src/main/java/com/pi/ativas/splash/SplashFragment.kt
@@ -1,21 +1,15 @@
 package com.pi.ativas.splash
 
-import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.findFragment
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.pi.ativas.R
 import com.pi.ativas.databinding.FragmentSplashBinding
-import com.pi.ativas.login.LoginFragment
 
 class SplashFragment : Fragment() {
 
