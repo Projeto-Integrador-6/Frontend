10aaa0e26771a3ade46f52156aa03da0d737a0a6
adailson4s <adailson.up@gmail.com>
2022-09-08 20:25:22 -0300

    Fluxo do primeiro login

--- app/src/main/java/com/pi/ativas/login/LoginFragment.kt
+++ app/src/main/java/com/pi/ativas/login/LoginFragment.kt
@@ -6,7 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
+import com.pi.ativas.R
 import com.pi.ativas.databinding.FragmentLoginBinding
+import kotlin.math.log
 
 class LoginFragment : Fragment() {
 
@@ -25,25 +30,46 @@ class LoginFragment : Fragment() {
         return binding.root
     }
 
-    private fun initViews(){
-        with(binding){
+    private fun initViews() {
+        with(binding) {
             btnLogar.setOnClickListener {
                 checkLogin()
             }
         }
     }
 
-    private fun checkLogin(){
+    private fun checkLogin() {
         // TODO: Deixar o botão "Entrar" clicavel apenas se login e senha inseridos!
-        with(binding){
-            if ((txtLogin.text.toString().isNotEmpty()) && (txtSenha.text.toString().isNotEmpty())){
-                Toast.makeText(requireContext(), "Favor inserir login e senha!", Toast.LENGTH_SHORT).show()
-            }
-            else {
-                Toast.makeText(requireContext(), "Login ou senha incorreto!", Toast.LENGTH_SHORT).show()
+        with(binding) {
+            val login = txtLogin.text.toString()
+            val password = txtSenha.text.toString()
+
+            if (login.isEmpty() || password.isEmpty()) {
+                Toast.makeText(requireContext(), "Favor inserir login e senha!", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                when (login) {
+                    "np" -> callNewPassword()
+                    "st" -> findNavController().navigate(R.id.action_loginFragment_to_homeStudentFragment)
+                    "tc" -> findNavController().navigate(R.id.action_loginFragment_to_homeTeacherFragment)
+                    else -> Toast.makeText(
+                        requireContext(),
+                        "Login ou senha incorreto!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             }
         }
+    }
 
+    private fun callNewPassword() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle("NOVA SENHA")
+            .setMessage("Por ser seu primeiro acesso ao UNI-Rank, você deve defenir uma nova senha!")
+            .setPositiveButton("OK") { dialog, which ->
+                findNavController().navigate(R.id.action_loginFragment_to_newPasswordFragment)
+            }
+            .show()
     }
 
 }
