3c4a965ca9aa2125f05f51d74ba1622419af8f0d
adailson4s <adailson.up@gmail.com>
2022-10-28 22:13:04 -0300

    Criação das telas para ver a atividade

--- app/src/main/java/com/pi/ativas/model/Task.kt
+++ app/src/main/java/com/pi/ativas/model/Task.kt
@@ -1,5 +1,9 @@
 package com.pi.ativas.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Task(
     val id: Int,
     val question: String,
@@ -8,10 +12,11 @@ data class Task(
     val pontuation: Int,
     val limit_date: String,
     val delivery_date: String?,
+    val question_title: String,
     val rank: Int,
     val reduction_factor: Float,
     val grouped: Int,
     val member_limiti: Int,
     val correction: Int,
     val class_id: Int
-)
+) : Parcelable
--- app/src/main/java/com/pi/ativas/student/ui/ViewTaskStudentFragment.kt
+++ app/src/main/java/com/pi/ativas/student/ui/ViewTaskStudentFragment.kt
@@ -1,24 +1,22 @@
-package com.pi.ativas.teacher.viewTask
+package com.pi.ativas.student.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.pi.ativas.base.BaseFragment
-import com.pi.ativas.databinding.FragmentViewTaskBinding
+import com.pi.ativas.databinding.FragmentViewTaskStudentBinding
 
-class ViewTaskFragment: BaseFragment() {
+class ViewTaskStudentFragment: BaseFragment() {
 
-
-    private lateinit var binding: FragmentViewTaskBinding
+    private lateinit var binding: FragmentViewTaskStudentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
-        binding = FragmentViewTaskBinding.inflate(layoutInflater)
+        binding = FragmentViewTaskStudentBinding.inflate(layoutInflater)
 
         return binding.root
     }
