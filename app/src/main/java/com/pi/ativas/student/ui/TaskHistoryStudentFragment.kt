a4f9353e63013f3f79c45c8214f69b0004e7f48d
adailson4s <adailson.up@gmail.com>
2022-11-15 14:27:19 -0300

    Implementação da tela de visualizar historico de tarefas

--- app/src/main/java/com/pi/ativas/student/ui/TaskHistoryStudentFragment.kt
+++ app/src/main/java/com/pi/ativas/student/ui/TaskHistoryStudentFragment.kt
@@ -1,16 +1,52 @@
 package com.pi.ativas.student.ui
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.pi.ativas.base.BaseFragment
+import com.pi.ativas.data.bodys.RequestTaskBody
 import com.pi.ativas.databinding.FragmentTaskHistoryStudentBinding
+import com.pi.ativas.model.Task
+import com.pi.ativas.student.adapter.ItemClickListener
+import com.pi.ativas.student.adapter.TaskAdapter
+import com.pi.ativas.student.viewmodel.HomeStudentViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class TaskHistoryStudentFragment : Fragment() {
+class TaskHistoryStudentFragment : BaseFragment() {
+
     private lateinit var binding: FragmentTaskHistoryStudentBinding
+    private lateinit var sharedPreferences: SharedPreferences
+    private val viewModel: HomeStudentViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sharedPreferences = requireContext().getSharedPreferences("dataLogin", Context.MODE_PRIVATE)
+
+        with(sharedPreferences) {
+            getString("email", "")?.let { email ->
+                getString("password", "")?.let { password ->
+                    getString("token", "")?.let { token ->
+                        viewModel.getTask(
+                            RequestTaskBody(
+                                email = email,
+                                password = password,
+                                token = token,
+                                classId = 0,
+                                //0 -> Todas as tarefas
+                                taskType = 0
+                            )
+                        )
+                    }
+                }
+            }
+        }
     }
 
     override fun onCreateView(
@@ -18,6 +54,47 @@ class TaskHistoryStudentFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding=FragmentTaskHistoryStudentBinding.inflate(layoutInflater)
+        initObservers()
         return binding.root
+    }
+
+    override fun initObservers() {
+        viewModel.listTask.observe(viewLifecycleOwner) {
+            if (it.isNotEmpty()) recycleView(it) else noTasks()
+            binding.progressbar.visibility = View.GONE
+            binding.bottomSheetBG.visibility = View.GONE
+        }
+
+        viewModel.error.observe(viewLifecycleOwner) {
+            binding.progressbar.visibility = View.GONE
+            binding.bottomSheetBG.visibility = View.GONE
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle("Erro $it")
+                .setMessage("Ocorreu um erro inesperado!")
+                .setPositiveButton("Ok") { dialog, which ->
+                }
+                .show()
+        }
+    }
+
+    private fun noTasks() {
+        binding.txtNoTasks.visibility = View.VISIBLE
+    }
+
+    private fun recycleView(taskList: List<Task>) {
+
+        val onClickListener = ItemClickListener { task ->
+            val action =
+                TaskHistoryStudentFragmentDirections.actionTaskHistoryStudentFragmentToViewTaskStudentFragment(task)
+            findNavController().navigate(action)
+        }
+
+        val recyclerView = binding.recycleView
+        val adapter = TaskAdapter(taskList, onClickListener)
+        val layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = adapter
     }
 }
